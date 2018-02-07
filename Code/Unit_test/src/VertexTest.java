8bf99f98c85f4cdf7895d863f707c6335231b383
msamu001 <msamu001@gold.ac.uk>
2018-02-07 22:56:12 +0000

    Updated Code
    Remade test to fit test driven development

---
+++ Code/Unit_test/src/VertexTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VertexTest {
+	Vertex vertex = new Vertex("test");
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
